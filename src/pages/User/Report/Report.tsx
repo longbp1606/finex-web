8b450f4da09d34adbf289f13d89f50fc11d12a9b
unknown <tnnn0320@gmail.com>
2025-02-14 04:39:01 -0800

    Change color of report page follow by theme - Fix line chart

--- src/pages/User/Report/Report.tsx
+++ src/pages/User/Report/Report.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
-import { CategoryItem, Container, DetailButton, Header, Section, StyledSection, StyledTable, StyledText, VerticalSection } from "./Report.styled";
-import { moneyData, weekOptions, weekSeries, reportColumns, reportData } from "./data";
+import { Amount, CategoryItem, Container, DetailButton, Header, Label, Section, StyledTable, StyledText, VerticalSection } from "./Report.styled";
+import { moneyData, weekSeries, reportColumns, reportData } from "./data";
 import { Progress, Select } from "antd";
 import Chart from "react-apexcharts";
 import DetailReport from "./DetailReport";
+import { theme } from "@/themes";
 
 const months = Array.from({ length: 12 }, (_, i) => ({
     label: `${i + 1}/2025`,
@@ -15,16 +16,33 @@ const MoneyCard: React.FC = () => {
         <div>
             {moneyData.map((data, index) => (
                 <Section key={index}>
-                    <h1>{data.amount}</h1>
-                    <p>{data.label}</p>
+                    <Amount>{data.amount}</Amount>
+                    <Label>{data.label}</Label>
                 </Section>
             ))}
         </div>
     );
 };
+
+const weekOptions = {
+    chart: {
+        toolbar: {
+            show: true, 
+            tools: {
+                pan: false, 
+                reset: false,
+                zoomin: true, 
+                zoomout: true, 
+                zoom: false, 
+                download: true, 
+            },
+        },
+    },
+};
+
 const WeekChart: React.FC = () => (
     <Section title="Last week's spending">
-        <Chart options={weekOptions} series={weekSeries} type="line" height={200} />
+        <Chart options={weekOptions} series={weekSeries} type="line" height={250} />
     </Section>
 );
 
@@ -32,28 +50,32 @@ const ReportTable = ({ setShowDetail }: { setShowDetail: React.Dispatch<React.Se
     const [selectedMonth, setSelectedMonth] = useState(months[0].value);
 
     return (
-        <StyledSection>
+        <Section title="Trading history">
             <Header>
                 <Select value={selectedMonth} onChange={setSelectedMonth} options={months} />
                 <DetailButton onClick={() => setShowDetail(true)}>Detail &gt;</DetailButton>
             </Header>
             <StyledTable columns={reportColumns} dataSource={reportData} pagination={false} />
-        </StyledSection>
+        </Section>
     );
 };
+
+const colors = ["#2A776A", `${theme.color.secondary}`, "#B0D1A2", `${theme.color.tertiary}`];
+const getRandomColor = () => colors[Math.floor(Math.random() * colors.length)];
+
 const TargetCategory: React.FC = () => (
     <Section title="Spending by Category">
         <CategoryItem>
             <StyledText>Food & Drinks</StyledText>
-            <Progress percent={82} strokeColor="#3B82F6" />
+            <Progress percent={82} strokeColor={getRandomColor()} />
         </CategoryItem>
         <CategoryItem>
             <StyledText>Shopping</StyledText>
-            <Progress percent={67} strokeColor="#3B82F6" />
+            <Progress percent={67} strokeColor={getRandomColor()} />
         </CategoryItem>
         <CategoryItem>
             <StyledText>Entertainment</StyledText>
-            <Progress percent={90} strokeColor="#3B82F6" />
+            <Progress percent={90} strokeColor={getRandomColor()} />
         </CategoryItem>
     </Section>
 );
@@ -64,7 +86,7 @@ const Report: React.FC = () => {
     return (
         <>
             {showDetail ? (
-                <DetailReport setShowDetail={setShowDetail}/>
+                <DetailReport setShowDetail={setShowDetail} />
             ) : (
                 <Container>
                     <VerticalSection>
