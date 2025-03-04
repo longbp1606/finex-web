b9c0433c7655f4563b8275461019465749eaf83f
Phan Long <longbp166@gmail.com>
2025-03-05 00:00:36 +0700

    feat: 🎸 chat bot

--- src/layouts/UserLayout/UserLayout.tsx
+++ src/layouts/UserLayout/UserLayout.tsx
@@ -7,6 +7,8 @@ import { Outlet, useNavigate } from "react-router-dom";
 import { SiGoogleanalytics } from "react-icons/si";
 import { BsRecordBtn } from "react-icons/bs";
 import { MenuFoldOutlined, MenuUnfoldOutlined } from '@ant-design/icons';
+import ChatAI from '@/components/ChatAI';
+
 
 const { Header, Content, Footer, Sider } = Layout;
 const { Title } = Typography;
@@ -35,38 +37,42 @@ const UserLayout = () => {
     };
 
     return (
-        <Layout className="min-h-screen">
-            <Sider collapsible collapsed={collapsed} className='bg-[#ecf4e9]' trigger={null}>
-                <div className="demo-logo-vertical" />
-                <Menu
-                    items={menuItems}
-                    selectedKeys={[keys[1]]}
-                    onSelect={handleMenuSelect}
-                    className='bg-[#ecf4e9]'
-                />
-            </Sider>
-            <Layout>
-                <Header className='p-0 bg-white flex items-center px-4'>
-                    {/* Button Toggle Sidebar */}
-                    <Button
-                        type="text"
-                        icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-                        onClick={() => setCollapsed(!collapsed)}
-                        style={{ fontSize: '16px', width: 64, height: 64 }}
+        <>
+            <Layout className="min-h-screen">
+                <Sider collapsible collapsed={collapsed} className='bg-[#ecf4e9]' trigger={null}>
+                    <div className="demo-logo-vertical" />
+                    <Menu
+                        items={menuItems}
+                        selectedKeys={[keys[1]]}
+                        onSelect={handleMenuSelect}
+                        className='bg-[#ecf4e9]'
                     />
-                    {/* Tên Menu Đang Chọn */}
-                    <Title level={4} className="ml-5" style={{marginBottom: "0px"}}>{selectedMenuLabel}</Title>
-                </Header>
-                <Content style={{ margin: '16px' }}>
-                    <Styled.ContentContainer>
-                        <Outlet />
-                    </Styled.ContentContainer>
-                </Content>
-                <Footer style={{ textAlign: 'center' }}>
-                    FiNex ©{new Date().getFullYear()} Created by Dunno Gang
-                </Footer>
+                </Sider>
+                <Layout>
+                    <Header className='p-0 bg-white flex items-center px-4'>
+                        {/* Button Toggle Sidebar */}
+                        <Button
+                            type="text"
+                            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+                            onClick={() => setCollapsed(!collapsed)}
+                            style={{ fontSize: '16px', width: 64, height: 64 }}
+                        />
+                        {/* Tên Menu Đang Chọn */}
+                        <Title level={4} className="ml-5" style={{ marginBottom: "0px" }}>{selectedMenuLabel}</Title>
+                    </Header>
+                    <Content style={{ margin: '16px' }}>
+                        <Styled.ContentContainer>
+                            <Outlet />
+                        </Styled.ContentContainer>
+                    </Content>
+                    <Footer style={{ textAlign: 'center' }}>
+                        FiNex ©{new Date().getFullYear()} Created by Dunno Gang
+                    </Footer>
+                </Layout>
             </Layout>
-        </Layout>
+
+            <ChatAI />
+        </>
     );
 };
 
